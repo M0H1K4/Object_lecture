057010e473500c752e58eed93041f7dabce9e950
M0H1K4 <nejer911@gmail.com>
2024-11-13 21:10:23 +0400

    add

---
+++ script.js
@@ -0,0 +1,68 @@
+ // var person = {
+    //     firstName: "Daviti",
+    //     age: 40,
+    //     country: "Georgia"
+    // }
+
+    // delete person.country
+    // console.log(person);
+    
+
+    // ახალი ელემენტის დამატება 
+    // person.birthYear = 1984;
+    // console.log(person.birthYear)
+    // console.log(person)
+
+
+    // არსებული მნიშვნელობის შეცვლა
+    // person.age = 35;
+    // console.log(person)
+
+
+
+
+    // const personFirstName = person.firstName
+    // const personAge = person.age
+    // const personCountry = person.country
+    // console.log(personFirstName)
+    // console.log(personAge);
+
+    // const arr = [personFirstName, personAge, personCountry]
+    
+    // console.log(arr);
+    
+
+    // function funqcia(num1, num2) {
+    //     return `summ of the two numbers is : ${num1 + num2}`
+    // }
+
+    // console.log(funqcia(10))
+
+    // console.log(funqcia(20, 100));
+
+
+    let person2 = {
+        firstName:"mirza",
+        age:"19",
+        lastName:"kapanadze",
+        arr: [14, 15, 20,100]
+
+    }
+    // console.log(person2.lastName)
+    console.log(person2.firstName, person2.lastName)
+    console.log(person2.arr);
+    
+
+    // alert(person2)
+    // prompt("what are u doing there?")
+
+    const car = {
+        carName: "Porsche 911",
+        model: "GT3 RS",
+        year: 2024
+    }
+
+    car.engine = "M113 K"
+    car.cylinder = "V8"
+
+    console.log(car)
